e3f1177d4ded665198ee4d6d5803e34f9d926993
somanita019 <somanita019@gmail.com>
2024-05-16 13:24:32 +0700

    resum with career

--- resources/views/menu-profile/resume.blade.php
+++ resources/views/menu-profile/resume.blade.php
@@ -1,1 +1,24 @@
-<?php
+@extends('layout.user')
+@section('contents')
+
+    <div class="bg-gray-100 font-sans">
+        <div class="container mx-auto px-4 py-8">
+            <div class="flex flex-col justify-center items-center my-16">
+                <h1 class="text-5xl font-bold text-gray-800 leading-tight">Choose Your Template</h1>
+                <p class="text-2xl text-gray-600 mt-4">Create your own resume with our template.</p>
+
+                <div class="flex justify-center mt-10 ">
+                    <a class="w-1/2 h-1/2 flex items-center justify-center">
+                        <img src="{{asset('/asset/resume1.jpeg')}}" class="object-contain h-full w-full">
+                    </a>
+                    <a href="{{route('/create_resume')}}" class="w-1/2 h-1/2 flex items-center justify-center">
+                        <img src="{{asset('/asset/resume2.jpeg')}}" class="object-contain h-full w-full">
+                    </a>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+@endsection
