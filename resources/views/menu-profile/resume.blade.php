6fe6a11751451141105aca543f4fa85f46fa9753
somanita019 <somanita019@gmail.com>
2024-05-21 09:40:48 +0700

    create-resume

--- resources/views/menu-profile/resume.blade.php
+++ resources/views/menu-profile/resume.blade.php
@@ -1,16 +1,20 @@
 @extends('layout.user')
 @section('contents')
-    <div class="bg-gray-100 font-sans">
+    <div class="bg-white font-sans">
             <div class="flex flex-col justify-center items-center my-16">
-                <h1 class="text-5xl font-bold text-gray-800 ">Choose Your Template</h1>
-                <p class="text-2xl text-gray-600 mt-4">Create your own resume with our template.</p>
+                <h1 class="text-4xl font-bold text-gray-800 ">Select a Template for your résume</h1>
+                <p class="text-l text-gray-600 mt-4">Worried about your design ? We have you covered. </p>
+                <p class="text-l text-gray-600 mt-4">Choose a layout you prefer and we will fill your information </p>
 
-                <div class="flex justify-center mt-10 ">
+                <div class="flex justify-center  ">
                     <a href="{{'/create_resume'}}" class="w-1/2  h-1/2 ">
-                        <img src="{{asset('/asset/resume1.jpeg')}}" class="object-cover ">
+                        <p class="text-4xl text-gray-600 mt-4 text-center mt-8">Profesional Blue</p>
+                        <img src="{{asset('/asset/resume1.jpeg')}}" class="object-cover mt-10 ">
                     </a>
                     <a href="{{'/create_resume'}}" class="w-1/2 h-1/2">
-                        <img src="{{asset('/asset/resume2.jpeg')}}" class="object-cover">
+                        <p class="text-4xl text-gray-600 mt-4 text-center mt-8">Contemperary </p>
+
+                        <img src="{{asset('/asset/resume2.jpeg')}}" class="object-cover mt-10">
                     </a>
                 </div>
 
